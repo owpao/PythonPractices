08263091a6881a57b248de9dedf9ff8d50966fde
John Paolo Flores <johnpaoloflores@gmail.com>
2018-07-08 11:47:17 +0800

    Finished Rock Paper Scissors Game

--- Exercises/Exercise8_RockPaperScissors.py
+++ Exercises/Exercise8_RockPaperScissors.py
@@ -6,40 +6,41 @@
 
 import random
 
+choices = ["Rock", "Paper", "Scissors"]
+gameStillGoing = bool(True)
+
 #capture input of Player
 print("Rock Paper Scissors Game!")
-playerInput = input("Choose Rock, Paper or Scissors?: ")
 
-print("Player chose {0}!".format(playerInput))
+while gameStillGoing:
+        playerInput = input("Choose Rock, Paper or Scissors?: ")
+        print("Player chose {0}!".format(playerInput))
 
-choices = ["Rock", "Paper", "Scissors"]
-enemyInput = choices[random.randint(0,2)]
-print("Enemy chose {0}!".format(enemyInput))
+        enemyInput = choices[random.randint(0,2)]
+        print("Enemy chose {0}!".format(enemyInput))
 
-if(playerInput == "Rock" and enemyInput == "Rock"):
-        print("Its a draw!")
-        break
-elif (playerInput == "Rock" and enemyInput == "Paper"):
-        print("You Lose")
-        break
-else:
-        print("You win!") 
-        break
-if(playerInput == "Paper" and enemyInput == "Paper"):
-        print("Its a draw!")
-        break
-elif (playerInput == "Paper" and enemyInput == "Scissors"):
-        print("You Lose")
-        break
-else:
-        print("You win!")
-        break
-if(playerInput == "Scissors" and enemyInput == "Scissors"):
-        print("Its a draw!")
-        break
-elif (playerInput == "Scissors" and enemyInput == "Rock"):
-        print("You Lose")
-        break
-else:
-        print("You win!")
 
+        if playerInput == enemyInput:
+                print("It's a tie!")
+        elif playerInput == "Rock":
+                if enemyInput == "Paper":
+                        print("You lose.")
+                else:
+                        print("You win!")
+        elif playerInput == "Paper":
+                if enemyInput == "Scissors":
+                        print("You lose.")
+                else:
+                        print("You Win!")
+        elif playerInput == "Scissors":
+                if enemyInput == "Rock":
+                        print("You lose!")
+                else:
+                        print("You Win!")
+        continuePlay = input("\n\nDo you want to play again? (Y/N): ")
+        if(continuePlay == 'Y'):
+                gameStillGoing = True
+        else:
+                gameStillGoing = False
+
+print("Thank you for playing! :)")
